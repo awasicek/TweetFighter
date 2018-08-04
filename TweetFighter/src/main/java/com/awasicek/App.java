bdc4e766485d46e2156d203414fdca5d6d1c82f5
Andrew Wasicek <awasicek@Andrews-MacBook-Pro.local>
2018-08-04 14:42:52 -0700

    generated maven archetype

---
+++ TweetFighter/src/main/java/com/awasicek/App.java
@@ -0,0 +1,13 @@
+package com.awasicek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
